8d8849318471084568c8475b33c0c40f0d55cd7a
aislaslo <islas.alex@gmail.com>
2025-01-09 15:38:50 -0600

    dawn of stockcandlestick

---
+++ main.py
@@ -0,0 +1,12 @@
+import yfinance as yf
+import mplfinance as mpf
+
+symbol = input("Enter stock symbol: ")
+start_date = '2022-01-01'
+end_date = '2025-01-01'
+
+aapl = yf.Ticker(symbol)
+stock_data = aapl.history(start=start_date, end=end_date)
+
+
+mpf.plot(stock_data, type='candle', style='yahoo', title=f'{symbol} Candlestick chart')
